0bbe4ea6b193aaa72108d9f5693c98bfbad2f02e
Jan Rochalski <joonny220@gmail.com>
2021-02-05 15:17:46 +0100

    chore: maxReturn param

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,7 +27,7 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
     client_secret: clientSecret
   });
 
-  const formsApiUrl = `https://${munchkinId}.mktorest.com/rest/asset/v1/forms.json${maxReturn ? `?${maxReturn}` : ''}`;
+  const formsApiUrl = `https://${munchkinId}.mktorest.com/rest/asset/v1/forms.json${maxReturn ? `?maxReturn=${maxReturn}` : ''}`;
   const authUrl = `https://${munchkinId}.mktorest.com/identity/oauth/token?${authOptions}`;
 
   try {
